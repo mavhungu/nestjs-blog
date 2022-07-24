e97df75a2a6761275e745e084912ddd9ba114f9b
ronewa <ronewamavhungu@gmail.com>
2022-07-24 20:25:48 +0200

    ..

--- backend-blog/src/app.module.ts
+++ backend-blog/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AdminModule } from './admin/admin.module';
       synchronize: true,
     }),
     AdminModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend-blog/src/app.service.ts
+++ backend-blog/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prismaService: PrismaService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+  getUser() {
+    return this.prismaService.user.findMany();
   }
 }
