8f2ee1645f884820240593baf9d649c6033ab61f
ronewa-mavhungu <ronewagilbert3@gmail.com>
2022-07-27 17:51:33 +0200

    adding post services

--- backend-blog/src/admin/post/post.service.ts
+++ backend-blog/src/admin/post/post.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor( private prismaService: PrismaService) {}
+
+  addPost(createPostDto: CreatePostDto) {
+    return this.prismaService.post.create({
+      data:{
+        title: createPostDto.title,
+        image: createPostDto.image,
+      }
+    })
+  }
+}
