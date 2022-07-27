aa194a6672720033f6e4b8fb43985dce739588ca
ronewa-mavhungu <ronewagilbert3@gmail.com>
2022-07-27 16:40:22 +0200

    ..

--- backend-blog/src/app.module.ts
+++ backend-blog/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
