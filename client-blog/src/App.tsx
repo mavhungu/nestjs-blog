c9f925bd6c6fe6901e1c78bb62ff9eee86a3644c
ronewa <ronewamavhungu@gmail.com>
2022-09-02 16:36:48 +0200

    remove login & register route

--- client-blog/src/App.tsx
+++ client-blog/src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Routes, Route, } from 'react-router-dom';
 import './App.css';
 import Ronewa from './ronewa';
-import Login from './login'
-import Register from './register'
-import Admin from './admin/index'
+import Admin from './admin/index';
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Ronewa/>} />
         <Route index element={<Ronewa />} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
         <Route path={'/admin'} element={<Admin/>} />
       </Routes>
     </div>
