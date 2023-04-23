0f3f8598117ac765e81784a5892fbb5c0eab4684
ronewa <ronewamavhungu@gmail.com>
2023-04-23 15:02:41 +0200

    change page not found

--- client-blog/src/pages/PageNotFound.tsx
+++ client-blog/src/pages/PageNotFound.tsx
@@ -4,9 +4,24 @@ import { useNavigate } from 'react-router-dom';
 const PageNotFound = ()=>{
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col justify-center align-center height-[100vh]">
-      <p>Page not found - 404</p>
-      <button onClick={()=>navigate(-1)}>Return back</button>
+    <div className="min-h-screen lg:p-24 md:py-20 md:px-44 py-324 flex justify-center items-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div className='xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0'>
+        <div className='relative'>
+          <div className='absolute'>
+            <div className="font-sans">
+              <h1 className='my-2 text-grey-800 font-bold text-2xl'>Page not found - 404</h1>
+              <p className="my-2 text-grey-800">Sorry fort hat!</p>
+              <button className='sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50' onClick={()=>navigate(-1)}>Return back</button>
+            </div>
+          </div>
+          <div className=''>
+           <img src='https://i.ibb.co/G9DC8S0/404-2.png' alt="ibb"/>
+          </div>
+        </div>
+      </div>
+      <div className='hidden md:block'>
+        <img src='https://i.ibb.co/ck1SGFJ/Group.png' alt='ck1'/>
+      </div>
     </div>
   )
 }
