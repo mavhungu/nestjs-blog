055f9a263314c156424f3b9f9c47ce74b6d89c30
ronewa <ronewamavhungu@gmail.com>
2023-05-10 12:15:39 +0200

    fixing padding bottom

--- client-blog/src/components/Aside.tsx
+++ client-blog/src/components/Aside.tsx
@@ -21,7 +21,7 @@ const Aside = ()=>{
             </div>
           </div>
 
-          <div className="flex items-center gap-x-6">
+          <div className="flex items-center gap-x-6 mt-2 border-b border-gray-200 pb-3">
             <img className="h-12 w-12 rounded-full ml-2" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="d" />
             <div>
               <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">Leslie Alexander</h3>
