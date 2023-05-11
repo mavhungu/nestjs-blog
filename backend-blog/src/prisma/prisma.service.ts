a0e2ab264318ecf8e931098804338658411f3257
ronewa <ronewamavhungu@gmail.com>
2023-05-11 16:31:25 +0200

    fixing prettier error

--- backend-blog/src/prisma/prisma.service.ts
+++ backend-blog/src/prisma/prisma.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
-
 @Injectable()
-export class PrismaService extends PrismaClient{
-  constructor(config: ConfigService){
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
     super({
-      datasources:{
+      datasources: {
         db: {
-          url: config.get("DATABASE_URL"),
+          url: config.get('DATABASE_URL'),
         },
       },
-    })
+    });
   }
 }
