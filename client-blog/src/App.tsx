f5a8a6b3979547a2072cb48191e5bd81c03ea6a8
ronewa <ronewamavhungu@gmail.com>
2023-05-16 12:13:14 +0200

    removing unused style

--- client-blog/src/App.tsx
+++ client-blog/src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route, } from 'react-router-dom';
 import { BlogPost, Home, SearchPost, PageNotFound } from'./pages';
 import { Dashboard } from './admin/pages/Dashboard';
 import Login from './admin/login';
 import Register from './admin/register';
+import { Spinner } from './components';
 
 function App() {
   return (
     <Routes>
       <Route path='/' />
-      <Route index element={<Home />} />
+      <Route index element={
+        <Suspense fallback={<Spinner/>}>
+          <Home />
+        </Suspense>
+        } 
+      />
       <Route path='blog-post'>
         <Route path={':id'} element={<BlogPost/>}/>
         <Route path={'search/:id'} element={<SearchPost/>} />
--- client-blog/src/index.tsx
+++ client-blog/src/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import "./interceptors/axios";
-//import "bootstrap/dist/css/bootstrap.min.css";
-//import "bootstrap/dist/js/bootstrap.bundle";
 import './index.css';
-import './css/general.css';
-import './css/style.css';
-import './css/queries.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
