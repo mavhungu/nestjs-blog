d47f130ee1e4319f71de80b96a8479e2d84cdef4
ronewa <ronewamavhungu@gmail.com>
2024-01-20 16:20:26 +0200

    adding links

--- client-blog/src/admin/components/Menu.tsx
+++ client-blog/src/admin/components/Menu.tsx
@@ -10,7 +10,7 @@ const Menu = () => {
       <div className="absolute lg:relative w-64 h-screen shadow bg-gray-100 hidden lg:block">
         <ul className="py-3">
           <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal pb-4 pt-4 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-            <div className="flex items-center">
+            <Link to="/admin/dashboard" className="flex items-center">
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" />
@@ -21,16 +21,16 @@ const Menu = () => {
                 </svg>
               </div>
                 <span className="ml-2">Dashboard</span>
-            </div>
+            </Link>
           </li>
           <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-4 mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-              <div className="flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" />
-                      <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                  </svg>
-                  <span className="ml-2">Products</span>
-              </div>
+            <Link to="/admin/create-post" className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" />
+                  <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
+              </svg>
+              <span className="ml-2">Products</span>
+            </Link>
           </li>
           <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
               <div className="flex items-center">
@@ -75,29 +75,29 @@ const Menu = () => {
               </div>
                 <ul className="py-3">
                   <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal pb-4 pt-4 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-                    <div className="flex items-center">
-                        <div className="w-6 h-6 md:w-8 md:h-8">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <rect x={4} y={4} width={6} height={6} rx={1} />
-                                <rect x={14} y={4} width={6} height={6} rx={1} />
-                                <rect x={4} y={14} width={6} height={6} rx={1} />
-                                <rect x={14} y={14} width={6} height={6} rx={1} />
-                            </svg>
-                        </div>
-                        <span className="ml-2 xl:text-base md:text-2xl text-base">Dashboard</span>
-                    </div>
+                    <Link to="/admin/dashboard" className="flex items-center">
+                      <div className="w-6 h-6 md:w-8 md:h-8">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-grid" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <rect x={4} y={4} width={6} height={6} rx={1} />
+                            <rect x={14} y={4} width={6} height={6} rx={1} />
+                            <rect x={4} y={14} width={6} height={6} rx={1} />
+                            <rect x={14} y={14} width={6} height={6} rx={1} />
+                        </svg>
+                      </div>
+                      <span className="ml-2 xl:text-base md:text-2xl text-base">Dashboard</span>
+                    </Link>
                   </li>
                   <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-4 mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-                    <div className="flex items-center">
-                        <div className="w-6 h-6 md:w-8 md:h-8">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
-                            </svg>
-                        </div>
-                        <span className="ml-2 xl:text-base md:text-2xl text-base">Products</span>
-                    </div>
+                    <Link to="/admin/create-post" className="flex items-center">
+                      <div className="w-6 h-6 md:w-8 md:h-8">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-puzzle" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                              <path stroke="none" d="M0 0h24v24H0z" />
+                              <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1" />
+                          </svg>
+                      </div>
+                      <span className="ml-2 xl:text-base md:text-2xl text-base">Products</span>
+                    </Link>
                   </li>
                   <li className="pl-6 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mb-4 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
                     <div className="flex items-center">
