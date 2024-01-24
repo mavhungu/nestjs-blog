410b05e3901609567fec486e86dd4298bcd7f7ad
ronewa <ronewamavhungu@gmail.com>
2024-01-24 10:54:06 +0200

    replacing " with ''

--- backend-blog/src/main.ts
+++ backend-blog/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ['http://localhost:3000'],
     credentials: true,
   });
   app.useGlobalPipes(
