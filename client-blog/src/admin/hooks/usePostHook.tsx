2634281ff5f204127eddeab4503200d7885e1155
ronewa <ronewamavhungu@gmail.com>
2024-01-24 10:09:08 +0200

    adding admin post hook

---
+++ client-blog/src/admin/hooks/usePostHook.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const usePostHook =()=> {
+  // const [slug, setSlug] = useState();
+  // cost [tagId, setTagId] = useState();
+
+  useEffect(() => {
+    
+    return () => {
+      
+    }
+  }, []);
+  
+  return (
+    <div>usePostHook</div>
+  )
+}
+
+export default usePostHook;
