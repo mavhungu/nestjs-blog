b4679d1618d26803ec00aec1c8d364a07c57d5b9
ronewa <ronewamavhungu@gmail.com>
2024-01-24 10:18:27 +0200

    adding default with credentials

--- client-blog/src/interceptors/axios.ts
+++ client-blog/src/interceptors/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
-
+axios.defaults.withCredentials = true;
 let refresh = false;
 
-axios.interceptors.response.use(resp => resp, async error => {
+axios.interceptors.response.use((resp) => resp, async error => {
   if (error.response.status === 401 && !refresh) {
     refresh = true;
     const response = await axios.post('refresh', {}, {withCredentials: true});
